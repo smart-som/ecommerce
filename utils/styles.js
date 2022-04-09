28585bf5b5223dcf2c00f96dd8fde793659951c8
smart-som <81562803+smart-som@users.noreply.github.com>
2022-04-09 16:34:18 +0100

    first commit

---
+++ utils/styles.js
@@ -0,0 +1,91 @@
+import { makeStyles } from '@material-ui/core';
+const useStyles = makeStyles((theme) => ({
+  navbar: {
+    backgroundColor: '#2E8BC0',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 10,
+    },
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1rem',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  main: {
+    minHeight: '80vh',
+  },
+  footer: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  form: {
+    width: '100%',
+    maxWidth: 800,
+    margin: '0 auto',
+  },
+  navbarButton: {
+    color: '#ffffff',
+    textTransform: 'initial',
+  },
+  transparentBackgroud: {
+    backgroundColor: 'transparent',
+  },
+  error: {
+    color: '#f04040',
+  },
+  fullWidth: {
+    width: '100%',
+  },
+  reviewForm: {
+    maxWidth: 800,
+    width: '100%',
+  },
+  reviewItem: {
+    marginRight: '1rem',
+    borderRight: '1px #808080 solid',
+    paddingRight: '1rem',
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  menuButton: { padding: 0 },
+  mt1: { marginTop: '1rem' },
+  // search
+  searchSection: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  searchForm: {
+    border: '1px solid #ffffff',
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+  },
+  searchInput: {
+    paddingLeft: 5,
+    color: '#000000',
+    '& ::placeholder': {
+      color: '#606060',
+    },
+  },
+  iconButton: {
+    backgroundColor: '#f8c040',
+    padding: 5,
+    borderRadius: '0 5px 5px 0',
+    '& span': {
+      color: '#000000',
+    },
+  },
+  sort: {
+    marginRight: 5,
+  },
+}));
+export default useStyles;
